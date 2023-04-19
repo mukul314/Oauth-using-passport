59fa3ef5089c80ddf239a8e54cfc6644857bde89
mukul314 <mukulsharma3779@gmail.com>
2023-04-19 06:51:11 +0530

    My first commit

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+//add this file to gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      "279587792447-v4q6saglodc7r37ad478sdkkq0aqbmm6.apps.googleusercontent.com",
+    clientSecret: "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",
+  },
+  mongodb: {
+    dbURI:
+      "mongodb+srv://mukulsharma3779:1234@cluster0.wa6jxoa.mongodb.net/test",
+  },
+  session:{
+    cookieKey: "mukulisanaawesomeboy"
+  }
+};
